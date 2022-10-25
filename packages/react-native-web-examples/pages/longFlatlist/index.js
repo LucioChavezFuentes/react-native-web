49563fd8c3d4e9eb4933cd03c08e73a433aa2d22
LucioChavezFuentes <luciobertinchavez@gmail.com>
2022-10-25 17:29:00 -0500

    add longFlatlist page

---
+++ packages/react-native-web-examples/pages/longFlatlist/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, Image } from 'react-native';
+
+const arrayData = [];
+for (let i = 0; i < 10000; i++) {
+  if (i % 2 === 0) {
+    arrayData.push({ item: i, type: 'text' });
+  } else {
+    arrayData.push({
+      item: 'https://uploads.codesandbox.io/uploads/user/6b7f9c8e-d7f1-4c55-b52f-c0c5c187c5f7/DOw--arbol+fuego.jpeg',
+      type: 'image'
+    });
+  }
+}
+
+function MyImage({ item }) {
+  return (
+    <Image
+      source={{
+        uri: item
+      }}
+      style={styles.imageStyle}
+    />
+  );
+}
+
+export default function LongFlatlist() {
+  return (
+    <FlatList
+      data={arrayData}
+      maxToRenderPerBatch={1}
+      renderItem={({ item: { item, type } }) => {
+        if (type === 'image') {
+          <MyImage item={item} />;
+        } else {
+          return <Text style={styles.listItemText}>{item}</Text>;
+        }
+      }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  listItemText: { fontSize: 16 },
+  imageStyle: { width: 200, height: 200 }
+});
