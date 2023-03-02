7bd736f12566190741a3298e2889c0679f147616
LucioChavezFuentes <luciobertinchavez@gmail.com>
2023-03-02 11:13:00 -0600

    ignore transformation on measure layout

--- packages/react-native-web/src/exports/UIManager/index.js
+++ packages/react-native-web/src/exports/UIManager/index.js
@@ -7,17 +7,25 @@
  * @noflow
  */
 
-import getBoundingClientRect from '../../modules/getBoundingClientRect';
 import setValueForStyles from '../../modules/setValueForStyles';
 
 const getRect = (node) => {
-  // Unlike the DOM's getBoundingClientRect, React Native layout measurements
-  // for "height" and "width" ignore scale transforms.
-  // https://developer.mozilla.org/en-US/docs/Web/API/CSS_Object_Model/Determining_the_dimensions_of_elements
-  const { x, y, top, left } = getBoundingClientRect(node);
-  const width = node.offsetWidth;
   const height = node.offsetHeight;
-  return { x, y, width, height, top, left };
+  const width = node.offsetWidth;
+  let left = node.offsetLeft;
+  let top = node.offsetTop;
+  node = node.offsetParent;
+
+  while (node && node.nodeType === 1 /* Node.ELEMENT_NODE */) {
+    left += node.offsetLeft + node.clientLeft - node.scrollLeft;
+    top += node.offsetTop + node.clientTop - node.scrollTop;
+    node = node.offsetParent;
+  }
+
+  top -= window.scrollY;
+  left -= window.scrollX;
+
+  return { width, height, top, left };
 };
 
 function hasLayoutBox(node) {
@@ -33,7 +41,7 @@ const measureLayout = (node, relativeToNativeNode, callback) => {
   if (node && relativeNode) {
     setTimeout(() => {
       if (hasLayoutBox(node) && hasLayoutBox(relativeNode)) {
-        const relativeRect = getBoundingClientRect(relativeNode);
+        const relativeRect = getRect(relativeNode);
         const { height, left, top, width } = getRect(node);
         const x = left - relativeRect.left;
         const y = top - relativeRect.top;
