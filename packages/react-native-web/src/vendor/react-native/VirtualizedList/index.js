fddc8c5c39c604107a4f76004678c41d5c49883f
LucioChavezFuentes <luciobertinchavez@gmail.com>
2023-03-02 19:49:32 -0600

    Revert "limit scroll by totalCellLength"
    This reverts commit 4f1c68187b27bce15e9a0e82f0fcb20af4e54c04.

--- packages/react-native-web/src/vendor/react-native/VirtualizedList/index.js
+++ packages/react-native-web/src/vendor/react-native/VirtualizedList/index.js
@@ -742,9 +742,9 @@ class VirtualizedList extends React.PureComponent<Props, State> {
       if (this.props.inverted && this._scrollRef && this._scrollRef.getScrollableNode) {
         const node = (this._scrollRef: any).getScrollableNode();
         if (this.props.horizontal) {
-          node.scrollLeft = Math.min(node.scrollLeft - (ev.deltaX || ev.wheelDeltaX) , this._totalCellLength)
+          node.scrollLeft -= ev.deltaX || ev.wheelDeltaX
         } else {
-          node.scrollTop = Math.min(node.scrollTop - (ev.deltaY || ev.wheelDeltaY) , this._totalCellLength)
+          node.scrollTop -= ev.deltaY || ev.wheelDeltaY
         }
         ev.preventDefault();
       }
